8158f55696b6f1fcd4dd68a199fceb42c02cf919
Erno Aapa <erno.aapa@gmail.com>
2017-09-27 09:29:40 +0300

    Updated hello-world example to echo if receives SIGINT to make testing easier

--- examples/hello-world/print-hello-world.sh
+++ examples/hello-world/print-hello-world.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# To test container attach, catch the SIGINT signal and 
+# echo something before closing so it's clear that it's working
+sigintHandler() {
+    echo "SIGINT received! I will stop the process now..."
+    exit
+}
+
+trap sigintHandler INT
+
 while [ 1 ]; do
     echo "Hello world!"
     sleep 1
