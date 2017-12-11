c93a52c23b07580f26b780cd62af35cd4321702d
Erno Aapa <erno.aapa@gmail.com>
2017-12-11 15:40:28 +0200

    Remove old unused legacy code

--- pkg/manifest/interface.go
+++
@@ -1,7 +0,0 @@
-package manifest
-
-// Source is interface for all state sources
-type Source interface {
-	Start()
-	Stop()
-}
--- pkg/state/interface.go
+++
@@ -1,6 +0,0 @@
-package state
-
-// Reporter sends information about current status
-type Reporter interface {
-	Start()
-}
