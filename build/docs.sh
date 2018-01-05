855d7111bc8df5a9a25b86e8c7322929f342258d
Erno Aapa <erno.aapa@gmail.com>
2018-01-05 15:26:34 +0200

    Fix docs

--- build/docs.sh
+++ build/docs.sh
@@ -3,17 +3,18 @@
 # Small script to build and update documentation to GitHub Pages
 # 
 
-npm install gitbook-plugin-insert-logo
-npm install gitbook-plugin-analytics
-npm install gitbook-plugin-terminal
-npm install gitbook-plugin-anchors
-npm install gitbook-plugin-github@2.0.0
+# npm install gitbook-plugin-insert-logo
+# npm install gitbook-plugin-analytics
+# npm install gitbook-plugin-terminal
+# npm install gitbook-plugin-anchors
+# npm install gitbook-plugin-github@2.0.0
 
 rm -rf _book
 gitbook install
 gitbook build
 cp docs/CNAME _book/CNAME
 cd _book
+touch .nojekyll
 git init
 git add -A
 git commit -m 'update book'
